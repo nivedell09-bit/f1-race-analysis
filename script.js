1c99cc9ee60c349dc427a8f6a77d83ad6bf9afd9
nivedell09@gmail.com <nivedell09@gmail.com>
2026-06-16 11:00:45 +0530

    Added F1 race simulation website

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-window.addEventListener("load", () => {
+let lap = 1;
 
-    const cards = document.querySelectorAll(".card");
+setInterval(() => {
 
-    cards.forEach((card, index) => {
-        card.style.opacity = "0";
-        card.style.transform = "translateY(30px)";
+    lap++;
 
-        setTimeout(() => {
-            card.style.transition = "all 1s";
-            card.style.opacity = "1";
-            card.style.transform = "translateY(0)";
-        }, index * 500);
-    });
+    if (lap > 57) {
+        lap = 1;
+    }
 
-});
+    document.getElementById("lapCounter").innerText = lap;
+
+}, 1000);
